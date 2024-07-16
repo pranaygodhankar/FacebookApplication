ed80f322b545791fcbba7fab0a665e9757b23601
HP <HP@DESKTOP-O76P9HF.domain.name>
2024-07-16 17:30:13 +0530

    Add user test cases by Pranay Automation Tester

---
+++ src/test/java/TutorialGit1/GitClass2.java
@@ -0,0 +1,12 @@
+package TutorialGit1;
+
+public class GitClass2 {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Add new user Test cases written by Pranay Automation Tester");
+
+	}
+
+}
